61be366bba3a906fadca068e881b06577ee745da
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-01-10 11:43:49 -0500

    Add API Filterer

--- demo/store/api_views.py
+++ demo/store/api_views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
 
 from store.serializers import ProductSerializer
 from store.models import Product
@@ -6,3 +7,5 @@ from store.models import Product
 class ProductList(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('id', )
