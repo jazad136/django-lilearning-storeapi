b974f3ff5bab5de8e70d1f1c0424728665a1d824
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-01-10 20:38:59 -0500

    Added on_sale filter

--- demo/store/api_views.py
+++ demo/store/api_views.py
@@ -9,3 +9,18 @@ class ProductList(ListAPIView):
     serializer_class = ProductSerializer
     filter_backends = (DjangoFilterBackend,)
     filter_fields = ('id', )
+
+    def get_queryset(self):
+        on_sale = self.request.query_params.get('on_sale', None)
+        if on_sale is None:
+            return super().get_queryset()
+        queryset = Product.objects.all()
+        if on_sale.lower() == 'true':
+            from django.utils import timezone
+            now = timezone.now()
+            return queryset.filter(
+                sale_start__lte=now,
+                sale_end__gte=now
+            )
+        return queryset
+            
