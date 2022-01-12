721e0853bab15acd69ff8dd99544530aedacdd08
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-01-12 11:56:23 -0500

    Adding Create Page

--- demo/store/api_views.py
+++ demo/store/api_views.py
@@ -1,8 +1,8 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
-
+from django.forms import ValidationError
 
 from store.serializers import ProductSerializer
 from store.models import Product
@@ -18,6 +18,7 @@ class ProductList(ListAPIView):
     filter_fields = ('id', )
     search_fields = {'name', 'description'} # used to map from the query
     pagination_class = ProductsPagination
+
     # parameters to the model fields.
 
     def get_queryset(self):
@@ -33,4 +34,14 @@ class ProductList(ListAPIView):
                 sale_end__gte=now
             )
         return queryset
-            
+        
+class ProductCreate(CreateAPIView):
+    serializer_class = ProductSerializer
+
+    def create(self, request, *args, **kwargs):
+        try:
+            price = request.data.get('price')
+            if price is not None and float(price) <= 0.0:
+                raise ValidationError({'price': 'Must be above $0.00' })
+        except ValueError:
+            raise ValidationError({'price': 'A valid number is required'})
